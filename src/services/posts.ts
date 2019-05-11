15f58a1a57d18389c2970c8ef9baaa6613f07a92
Anu Dhagat <anudhagat@gmail.com>
2019-05-11 13:19:31 -0700

    add functions to fetch posts users and comments from api

---
+++ src/services/posts.ts
@@ -0,0 +1,27 @@
+import fetch from 'isomorphic-fetch';
+
+const BASE_API_URL = 'https://jsonplaceholder.typicode.com';
+
+export function fetchPosts() {
+  return fetch(`${BASE_API_URL}/posts`)
+    .then((response: any) => response.json())
+    .catch((err: any) => err);
+}
+
+export function fetchUsers() {
+  return fetch(`${BASE_API_URL}/users`)
+    .then((response: any) => response.json())
+    .catch((err: any) => err);
+}
+
+export function fetchCommentsByPostId(postId: number) {
+  return fetch(`${BASE_API_URL}/comments/postId=${postId}`)
+    .then((response: any) => response.json())
+    .catch((err: any) => err);
+}
+
+export function fetchPostById(postId: number) {
+  return fetch(`${BASE_API_URL}/posts/${postId}`)
+    .then((response: any) => response.json())
+    .catch((err: any) => err);
+}
