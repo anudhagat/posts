08163ab4e70e0cd8b853a1a7b06ee06a279630e9
Anu Dhagat <anudhagat@gmail.com>
2019-05-11 21:45:45 -0700

    connect posts-view to the store

--- src/views/posts-view/index.tsx
+++ src/views/posts-view/index.tsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import fetchPostsAction from '../../models/posts/posts-actions';
+import { Post } from '../../models/posts/types';
+import { AppState } from '../../models/reducers';
+import PostList from '../../components/post-list';
 
-function PostsView() {
-  return <div>PostsView</div>;
+interface StateProps {
+  posts: Post[];
+  loading: boolean;
+  error: string;
 }
-export default PostsView;
+
+interface DispatchProps {
+  fetchPosts: () => void;
+}
+
+type Props = StateProps & DispatchProps;
+
+export class PostsView extends Component<Props> {
+  componentDidMount() {
+    const { fetchPosts } = this.props;
+    if (fetchPosts) {
+      fetchPosts();
+    }
+  }
+  render() {
+    const { posts } = this.props;
+    return <PostList posts={posts} />;
+  }
+}
+
+const mapStateToProps = (state: AppState) => ({
+  posts: state.posts.posts,
+});
+
+const mapDispatchToProps = {
+  fetchPosts: fetchPostsAction,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PostsView);
