40c2ba265530abc70c3fc90d46029656a2b6888b
Anu Dhagat <anudhagat@gmail.com>
2019-05-12 23:42:14 -0700

    add search input and autocomplete dropdown

--- src/views/users-view/index.tsx
+++ src/views/users-view/index.tsx
@@ -4,6 +4,7 @@ import { AppState } from '../../models/reducers';
 import UserList from '../../components/user-list';
 import { fetchUsersAction } from '../../models/users/users-actions';
 import { User } from '../../models/users/types';
+import './styles.css';
 
 interface StateProps {
   users: User[];
@@ -18,15 +19,69 @@ interface DispatchProps {
 type Props = StateProps & DispatchProps;
 
 export class UsersView extends Component<Props> {
+  state = {
+    users: this.props.users,
+  };
+
   componentDidMount() {
     const { fetchUsers } = this.props;
+
     if (fetchUsers) {
       fetchUsers();
     }
   }
+
+  componentDidUpdate(prevProps: Props) {
+    const { users } = this.props;
+    if (
+      prevProps.users.length === 0 &&
+      prevProps.users.length !== users.length
+    ) {
+      this.setState({ users });
+    }
+  }
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const textToFilter = e.target.value.toLowerCase();
+    const { users } = this.props;
+    let filteredUsers = [];
+    if (textToFilter !== '') {
+      filteredUsers = users.filter(user => {
+        const userName = user.username.toLowerCase();
+        const userFullName = user.name.toLowerCase();
+        return (
+          userName.includes(textToFilter) || userFullName.includes(textToFilter)
+        );
+      });
+    } else {
+      filteredUsers = [...users];
+    }
+    this.setState({ users: filteredUsers });
+  };
+
   render() {
-    const { users, loading } = this.props;
-    return <UserList users={users} loading={loading} />;
+    const { loading } = this.props;
+    const { users } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          list="searchList"
+          onChange={this.handleChange}
+          placeholder="Search User ..."
+          className="searchInput"
+        />
+        <datalist id="searchList">
+          {users.map(user => (
+            <option
+              key={user.id}
+              value={`Name: ${user.name} UserName: ${user.username}`}
+            />
+          ))}
+        </datalist>
+        <UserList users={users} loading={loading} />
+      </div>
+    );
   }
 }
 
