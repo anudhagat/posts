a3fe8c7618c11a89251accd9a7b5cb77c89055eb
Anu Dhagat <anudhagat@gmail.com>
2019-05-12 22:07:55 -0700

    add new route to link to user detail view

--- src/components/user-list/user-list-item.tsx
+++ src/components/user-list/user-list-item.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 import { User } from '../../models/users/types';
 
@@ -8,10 +9,19 @@ interface Props {
 
 function UserListItem({ user }: Props) {
   return (
-    <tr className="userListItem">
-      <td className="userName">{user.username}</td>
-      <td className="name">{user.name}</td>
-    </tr>
+    <li className="userListItem">
+      <Link
+        to={{
+          pathname: `/user/${user.id}`,
+          state: {
+            user,
+          },
+        }}
+      >
+        <span className="userName">{user.username}</span>
+        <span className="name">{user.name}</span>
+      </Link>
+    </li>
   );
 }
 export default UserListItem;
--- src/views/layout/index.tsx
+++ src/views/layout/index.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import HeaderNav from '../../components/header-nav';
 import PostsView from '../posts-view';
 import PostDetailView from '../post-detail-view';
+import UserDetailView from '../user-detail-view';
 import UsersView from '../users-view';
 
 function Layout() {
@@ -14,6 +15,7 @@ function Layout() {
           <Route exact path="/" component={PostsView} />
           <Route path="/users" component={UsersView} />
           <Route path="/post/:postId" component={PostDetailView} />
+          <Route path="/user/:userId" component={UserDetailView} />
         </Switch>
       </div>
     </div>
